aac68291c2035d6ec4ca1cf12f57cd1d859ed3a6
John Shelley <john.shelley.p@gmail.com>
2015-11-08 16:28:53 -0600

    Add item layout for MyViewHolder and adapter

--- app/src/main/java/last/first/hudlu/MyAdapter.java
+++ app/src/main/java/last/first/hudlu/MyAdapter.java
@@ -2,8 +2,10 @@ package last.first.hudlu;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
@@ -18,12 +20,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_class2, parent, false);
+        return new MyViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        holder.mMyText.setText(mDataset[position]);
     }
 
     @Override
@@ -33,8 +37,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView mMyText;
         public MyViewHolder(View v) {
             super(v);
+            mMyText = (TextView) v.findViewById(R.id.item_my_text);
         }
     }
 
